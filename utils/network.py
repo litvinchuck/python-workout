bfdc56e36171c10f3a8b881a22a7dd8425ecda0b
Edward Litvinchuck <elitvinchuck@gmail.com>
2017-01-03 14:42:21 +0200

    network: network io statistics

---
+++ utils/network.py
@@ -0,0 +1,53 @@
+"""Networking functions"""
+from socket import socket, gethostbyname
+
+
+def scan_port(host, port):
+    """Scans if port is open on specified host.
+    Args:
+        host(str) - host name or host ip
+        port(int) - port number
+
+    Returns:
+        True if port is open, False otherwise
+    """
+    socket_instance = socket()
+    host_ip = gethostbyname(host)
+    return socket_instance.connect_ex((host_ip, port)) == 0
+
+
+def network_io():
+    """Returns Linux environment network io statistics in dictionary format.
+    Where dictionary keys are device names, dictionary values are tuples containing integer values in following order:
+        number of bytes sent,
+        number of bytes received,
+        number of packets sent,
+        number of packets received,
+        number of errors while receiving,
+        number of errors while sending,
+        numbers of dropped incoming packets,
+        number of dropped outgoing packets
+    """
+    results_dict = {}
+    with open('/proc/net/dev') as file:
+        lines = file.readlines()
+    for line in lines[2:]:
+        colon = line.find(':')
+        name = line[:colon].strip()
+        stats = line[colon + 1:].split()
+
+        (bytes_recieved, packets_received, err_in, drops_in, fifo_in, frame_in, compressed_in, multicast_in,
+            bytes_sent, packets_sent, err_out, drops_out, fifo_out, collisions_out, carrier_out,
+            compressed_out) = map(int, stats)
+
+        results_dict[name] = (bytes_sent, bytes_recieved, packets_sent, packets_received, err_in, err_out, drops_in,
+                              drops_out)
+
+    return results_dict
+
+
+if __name__ == '__main__':
+    assert scan_port('google.com', 80)
+    assert scan_port('ftp.debian.org', 80)
+    assert scan_port('ftp.debian.org', 21)
+    assert scan_port('ftp.debian.org', 22)
